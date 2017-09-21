58fc79439635435a8306ed8f4bb1f2a944349da1
Indrani <indraaani@gmail.com>
2017-09-21 23:23:46 +0530

    ex31 making decisions | fixed minor errors

--- exercises/ex31.py
+++ exercises/ex31.py
@@ -7,7 +7,7 @@ door = raw_input("> ")
 if door == "1":
     print "There's a giant bear here eating a cheese cake. What do you do?"
     print "1. Take the cake."
-    print "Scream at the bear."
+    print "2. Scream at the bear."
 
     bear = raw_input("> ")
 
