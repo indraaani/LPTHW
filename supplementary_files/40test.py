7f3de5d47066c5648569d107e4054983d4f758e2
Indrani <indraaani@gmail.com>
2017-11-07 13:57:19 +0530

    ex40 supplementary files

---
+++ supplementary_files/40test.py
@@ -0,0 +1,16 @@
+import mystuff
+
+# mystuff.apple()
+# print mystuff.tangerine
+
+# dict style
+mystuff['apples']
+
+# module style
+mystuff.apples()
+print mystuff.tangerine
+
+# class style
+thing = MyStuff()
+thing.apples()
+print thing.tangerine
